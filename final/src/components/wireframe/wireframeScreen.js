c12bf40de02146a1a7505e86ef427b8658d9ad3f
Tianhao Xi <tianhaoxi@Tianhaos-MacBook-Pro.local>
2019-12-13 18:41:28 -0500

    frame css

--- final/src/components/wireframe/wireframeScreen.js
+++ final/src/components/wireframe/wireframeScreen.js
@@ -12,9 +12,61 @@ class WireframeScreen extends Component {
     render() {
 
         return (
-            <div className="container grey lighten-2">
-                hahaha
-            </div>
+            
+            <div className="todo_home">
+
+                <div className="left_container">
+                <div className="button_container">
+                <button type="button" className="material-icons " 
+                    >zoom_in</button>
+                    <button type="button" className="material-icons "
+                    >zoom_out</button>
+                    <button type="button" className="waves-effect waves-light btn "
+                    >&#10005;</button>
+                    <button type="button" className="waves-effect waves-light btn "
+                    >&#10005;</button>
+                </div>
+
+                <div className="wire_container">
+                    
+                <div className="standard_container">
+                </div>
+                <label for="standard_container">container</label>
+
+                <h6 className="prompt_for_input">Prompt for input:</h6>
+                <label for="prompt_for_input">Lable</label>
+
+                <button type="button" className="waves-effect waves-light btn-small " id="show_button">submit</button>
+                <label for="waves-effect waves-light btn ">Button</label>
+
+
+                <input type="text" className="browser-default" value="input" id="Textfield_default"></input> 
+                <label for="Textfield_default">Textfield</label>
+               
+
+                </div>
+                </div>
+
+                <div className="middle_container"></div>
+
+
+
+
+
+                <div className="property_container">
+                <p>Properties</p>
+                <button type="button" className="waves-effect waves-light btn-small " id="property_btn">submit</button>
+                <p>Font size: 
+                <input type="text" className="browser-default" value="input" id="font_input"></input> 
+                </p>
+
+
+
+                </div>
+               
+
+                </div>
+            
         )
     }
 }
